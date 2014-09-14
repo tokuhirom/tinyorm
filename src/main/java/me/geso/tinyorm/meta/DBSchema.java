7cbfe770f1ea3385fd5953f1f29ba9edc61607b3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-15 06:33:50 +0900

    Added shortcut for triggers.

--- src/main/java/me/geso/tinyorm/meta/DBSchema.java
+++ src/main/java/me/geso/tinyorm/meta/DBSchema.java
@@ -2,22 +2,53 @@ package me.geso.tinyorm.meta;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import lombok.extern.slf4j.Slf4j;
 import me.geso.tinyorm.Row;
+import me.geso.tinyorm.trigger.BeforeInsertHandler;
+import me.geso.tinyorm.trigger.BeforeUpdateHandler;
+import me.geso.tinyorm.trigger.Deflater;
+import me.geso.tinyorm.trigger.Inflater;
 
+@Slf4j
 public class DBSchema {
 	private static ConcurrentHashMap<Class<? extends Row>, TableMeta> registry = new ConcurrentHashMap<>();
-	
+
 	public void registerClass(Class<? extends Row> klass) {
 		TableMeta meta = TableMeta.build(klass);
+		log.info("Loaded " + klass);
 		registry.put(klass, meta);
+	}
+
+	public void addBefore(Class<? extends Row> klass,
+			BeforeInsertHandler handler) {
+		TableMeta tableMeta = this.getTableMeta(klass);
+		tableMeta.addBeforeInsertHandler(handler);
+	}
+
+	public void addBeforeUpdateHandler(Class<? extends Row> klass,
+			BeforeUpdateHandler handler) {
+		TableMeta tableMeta = this.getTableMeta(klass);
+		tableMeta.addBeforeUpdateHandler(handler);
+	}
+
+	public void addDeflater(Class<? extends Row> klass, Deflater deflater) {
+		TableMeta tableMeta = this.getTableMeta(klass);
+		tableMeta.addDeflater(deflater);
+	}
+
+	public void addInflater(Class<? extends Row> klass, Inflater inflater) {
+		TableMeta tableMeta = this.getTableMeta(klass);
+		tableMeta.addInflater(inflater);
 	}
 
 	public TableMeta getTableMeta(Class<? extends Row> klass) {
 		TableMeta meta = registry.get(klass);
 		if (meta == null) {
-			throw new RuntimeException("Unknown row class: " + klass);
+			log.info("Loading " + klass);
+			meta = TableMeta.build(klass);
+			registry.put(klass, meta);
 		}
 		return meta;
 	}
-	
+
 }
