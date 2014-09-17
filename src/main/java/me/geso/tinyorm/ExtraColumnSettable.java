91ef866610cd9928f73a1455bc70a558ccb8bc08
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-17 13:30:53 +0900

    Remove broken @see tag

--- src/main/java/me/geso/tinyorm/ExtraColumnSettable.java
+++ src/main/java/me/geso/tinyorm/ExtraColumnSettable.java
@@ -3,7 +3,6 @@ package me.geso.tinyorm;
 /**
  * If there is an extra columns, I can eat extra columns.<br>
  * 
- * @see Row.setExtraColumn
  */
 public interface ExtraColumnSettable {
 	public void setExtraColumn(String name, Object value);
--- src/main/java/me/geso/tinyorm/annotations/CsvColumn.java
+++ src/main/java/me/geso/tinyorm/annotations/CsvColumn.java
@@ -6,8 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @see me.geso.tinyorm.TableMeta
- * @author tokuhirom
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
