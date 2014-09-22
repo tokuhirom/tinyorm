2cb5d3a3696c13385c71265d7fb6c8dba6ec433d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-22 16:18:41 +0900

    Suppress warnings

--- src/main/java/me/geso/tinyorm/TinyORM.java
+++ src/main/java/me/geso/tinyorm/TinyORM.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -425,7 +424,6 @@ public class TinyORM {
 	 * Execute query.
 	 * 
 	 * @param sql
-	 * @param params
 	 * @param callback
 	 * @return
 	 * @throws RichSQLException
