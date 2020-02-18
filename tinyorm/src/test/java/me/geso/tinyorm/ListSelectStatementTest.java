f0ac8fd75fd7fe4781bd41306481eb96348d4114
masahiro-yamashita <masahiro.yamashita@linecorp.com>
2020-02-18 13:29:43 +0900

    Revert ListSelectStatement#executeWithPagination for backward-compatibility

--- tinyorm/src/test/java/me/geso/tinyorm/ListSelectStatementTest.java
+++ tinyorm/src/test/java/me/geso/tinyorm/ListSelectStatementTest.java
@@ -1,7 +1,7 @@
 package me.geso.tinyorm;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,6 +21,41 @@ public class ListSelectStatementTest extends TestBase {
         createTable("member",
                     "id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY",
                     "name VARCHAR(255) NOT NULL");
+    }
+
+    @Test
+    public void testExecuteWithPagination() {
+        // empty result
+        {
+            Paginated<Member> list = orm.search(Member.class).orderBy("id DESC").executeWithPagination(1);
+            assertThat(list.getRows().size(), is(0));
+            assertThat(list.getEntriesPerPage(), is(1L));
+            assertThat(list.getHasNextPage(), is(false));
+        }
+        for (int i = 0; i<3; i++) {
+            orm.insert(Member.class).value("name", "name").execute();
+        }
+        // first page
+        {
+            Paginated<Member> list = orm.search(Member.class).orderBy("id DESC").executeWithPagination(1);
+            assertThat(list.getRows().size(), is(1));
+            assertThat(list.getEntriesPerPage(), is(1L));
+            assertThat(list.getHasNextPage(), is(true));
+        }
+        // last page
+        {
+            Paginated<Member> list = orm.search(Member.class).orderBy("id DESC").executeWithPagination(3);
+            assertThat(list.getRows().size(), is(3));
+            assertThat(list.getEntriesPerPage(), is(3L));
+            assertThat(list.getHasNextPage(), is(false));
+        }
+        // last page (less than entries per page)
+        {
+            Paginated<Member> list = orm.search(Member.class).orderBy("id DESC").executeWithPagination(4);
+            assertThat(list.getRows().size(), is(3));
+            assertThat(list.getEntriesPerPage(), is(4L));
+            assertThat(list.getHasNextPage(), is(false));
+        }
     }
 
     @Test
